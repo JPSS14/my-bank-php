ced0b1555ed17e5fc0934dd1dd6370c16dbd1ffc
JPSS14 <jp_scarabelli@hotmail.com>
2021-02-01 17:12:47 -0300

    UPDATE 3: Create register page and added form standard to boot

--- Themes/myBank/cadastrar.php
+++ Themes/myBank/cadastrar.php
@@ -1,11 +1,12 @@
 <div class="standard-form-div">
-    <header class="standard-form-div-header">
-        <h1>Login</h1>
+    <header class="standard-form-header">
+        <h1 class="form-div-header-h1">Cadastrar</h1>
     </header>
     <form class="standard-form">
+        <input type="text" name="nome" placeholder="Nome">
+        <input type="text" name="sobrenome" placeholder="Sobrenome">
         <input type="email" name="email" placeholder="Email">
         <input type="password" name="senha" placeholder="Senha">
         <input type="submit" class="btn" value="Entrar">
     </form>
-    <a class="btn" href="<?= "{$configBase}cadastrar" ?>">Cadastrar</a>
 </div>
--- index.php
+++ index.php
@@ -18,6 +18,7 @@ $configSiteName = "mybank";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= $configBase; ?>Assets/Styles/fonticon.css">
     <link rel="stylesheet" href="<?= $configBase; ?>Assets/Styles/boot.css">
     <link rel="stylesheet" href="<?= $configThemeLink . "/style.css" ?>">
