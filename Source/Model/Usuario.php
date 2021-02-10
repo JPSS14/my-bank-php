43ede8cd6de7dd1ad564ed4e8e868d55b4357964
JPSS14 <jp_scarabelli@hotmail.com>
2021-02-09 23:38:14 -0300

    UPDATE 6: Create Model and Usuario struct

--- Source/Model/Usuario.php
+++ Source/Model/Usuario.php
@@ -2,3 +2,55 @@
 
 namespace Source\Model;
 
+class Usuario extends Model
+{
+    protected static $safe = ["id", "created_at", "updated_at"];
+
+    protected static $entity = "usuarios";
+
+    public function bootstrap(string $nome, string $sobrenome, string $email, string $senha): ?Usuario
+    {
+        $this->nome = $nome;
+        $this->sobrenome = $sobrenome;
+        $this->email = $email;
+        $this->senha = $senha;
+
+        return $this;
+    }
+
+    public function load(int $id, string $columns = "*"): ?Usuario
+    {
+        return $this;
+    }
+
+    public function find($email, string $columns = "*"): ?Usuario
+    {
+
+        return $this;
+    }
+
+    public function all(int $limit = 30, int $offset = 0, string $columns = "*"): ?array
+    {
+        
+
+        return [];
+    }
+
+    public function save(): ?Usuario
+    {
+      
+        return $this;
+    }
+
+    public function destroy(): ?Usuario
+    {
+       
+       return $this;
+    }
+
+    private function required(): ?bool
+    {
+        
+        return true;
+    }
+}
